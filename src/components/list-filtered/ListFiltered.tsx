30dbb3fe77ec6e1ec111507e336c1458c367921b
Dinael URDANETA <dinael@gmail.com>
2023-08-24 11:50:45 +0200

    🔨 ListFiltered: some props changed

--- src/components/list-filtered/ListFiltered.tsx
+++ src/components/list-filtered/ListFiltered.tsx
@@ -21,47 +21,43 @@ export type ListFilteredItem = {
 export type ListFilteredProps = {
   items: ListFilteredItem[]
   nameList?: string
-  direction?: 'vertical' | 'horizontal'
   className?: string
   children?: ReactNode
   OnlySearch?: boolean
   OnlySort?: boolean
-  bulletList?: 'none' | 'auto' | 'disc' | 'circle' | 'square' | 'decimal' | 'decimal-leading-zero' | 'lower-roman' | 'upper-roman' | 'lower-greek' | 'lower-latin' | 'upper-latin' | 'armenian' | 'georgian' | 'lower-alpha' | 'upper-alpha'
+  bullet?: 'none' | 'auto' | 'disc' | 'circle' | 'square' | 'decimal' | 'decimal-leading-zero' | 'lower-roman' | 'upper-roman' | 'lower-greek' | 'lower-latin' | 'upper-latin' | 'armenian' | 'georgian' | 'lower-alpha' | 'upper-alpha'
 }
 
 export const ListFiltered: FC<ListFilteredProps> = ({
   items,
   nameList,
-  direction ='vertical',
   className = '',
   OnlySearch = false,
   OnlySort = false,
-  bulletList,
+  bullet,
   children,
   ...props
 }) => {
-
-  const [searchQuery, setSearchQuery] = useState('')
-  const [sortOption, setSortOption] = useState('top')
-  const [alphabeticalOrder, setAlphabeticalOrder] = useState<'asc' | 'desc'>('asc');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortOption, setSortOption] = useState('top');
 
   if (!items) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   const filteredItems = items.filter(
     (item) =>
       item.text.toLowerCase().includes(searchQuery.toLowerCase())
-  )
+  );
 
   const sortedItems = [...filteredItems].sort((a, b) => {
     if (sortOption === 'alphabetically_asc') {
-      return a.text.localeCompare(b.text)
+      return a.text.localeCompare(b.text);
     } else if (sortOption === 'alphabetically_desc') {
-      return b.text.localeCompare(a.text)
+      return b.text.localeCompare(a.text);
     }
-    return 0
-  })
+    return 0;
+  });
 
   const handleSortChange = (event: ChangeEvent<HTMLSelectElement>) => {
     setSortOption(event.target.value as typeof sortOption);
@@ -80,15 +76,16 @@ export const ListFiltered: FC<ListFilteredProps> = ({
       option: 'Z to A',
       value: 'alphabetically_desc'
     }
-  ]
+  ];
+
+  const showList = sortedItems.length > 0;
 
   return (
     <div
       className={`
-        ${styles['list-filtered-component']}
-        ${direction === 'horizontal' ? styles.horizontal : ''}
-        ${className && className}
-      `}
+      ${styles['list-filtered-component']}
+      ${className && className}
+    `}
       {...props}>
       <ListFilters
         className={`${styles['list-filter']}`}
@@ -103,20 +100,22 @@ export const ListFiltered: FC<ListFilteredProps> = ({
         onlySearch={OnlySearch}
         onlySort={OnlySort}
       />
-      {sortedItems.length > 0
-        ? <List
-          items={sortedItems}
-          aria-live='polite'
-          ordered
-          nameList={nameList ? nameList : `list-${uuid()}`}
-          bullet={bulletList ? bulletList : 'none'}
-        />
-        : <ListFilteredNotFound
-          className={`${styles['list-not-found']}`}
-          message='Search not found'
-        />
-
-      }
+      {children || (
+        showList ? (
+          <List
+            items={sortedItems}
+            aria-live='polite'
+            ordered
+            nameList={nameList ? nameList : `list-${uuid()}`}
+            bullet={bullet ? bullet : 'none'}
+          />
+        ) : (
+          <ListFilteredNotFound
+            className={`${styles['list-not-found']}`}
+            message='Search not found'
+          />
+        )
+      )}
     </div>
   );
 };
