6a2bbd831b97f1cce750dd9acddcc445345cd529
Dinael URDANETA <dinael@gmail.com>
2023-08-24 08:45:06 +0200

    🔨 InputField: fix className logic

--- src/components/inputfield/Inputfield.tsx
+++ src/components/inputfield/Inputfield.tsx
@@ -72,7 +72,8 @@ export const Inputfield: FC<InputfieldProps> = ({
       htmlFor={id}
       className={`
         ${styles['inputfiled-component']}
-        ${error? styles['has-error'] : ''}
+        ${error ? styles['has-error'] : ''}
+        ${className ? className : ''}
       `}
       {...props}>
       <LabelText
