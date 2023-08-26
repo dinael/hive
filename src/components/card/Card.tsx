04d80add02f200e4e437b0998889f5582fde49b4
Dinael URDANETA <dinael@gmail.com>
2023-08-26 08:18:00 +0200

    🧩 New composed component: Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,23 +1,40 @@
 import { FC, ReactNode, isValidElement, Children } from 'react';
 
-import TitleCard from '../title/Title'
+import styles from './Card.module.scss'
+
+import CardTitle from '../title/Title'
+import CardImg from '../image/Image'
 
 type SlotMap = {
   [key: string]: ReactNode[];
 };
 
-type CardProps = {
-  Title: string
-  TitleLevel: 2 | 3 | 4 | 5 | 6
+export type CardProps = {
+  title: string
+  titleLevel: 2 | 3 | 4 | 5 | 6
+  imgSrc?: string
+  imgAlt?: string
+  className: string
+  unstyled: boolean
   children: ReactNode;
 };
 
-const Card: FC<CardProps> = ({
-  Title,
-  TitleLevel = 3,
-  children
+export const Card: FC<CardProps> = ({
+  title,
+  titleLevel = 3,
+  imgSrc,
+  imgAlt = '',
+  className,
+  unstyled,
+  children,
+  ...props
 }) => {
   const slotMap: SlotMap = {};
+
+  const cardStyles = `
+    ${!unstyled ? styles['card-component'] : ''}
+    ${className || ''}
+  `
 
   Children.forEach(children, child => {
     if (isValidElement(child)) {
@@ -27,25 +44,42 @@ const Card: FC<CardProps> = ({
   });
 
   return (
-    <article className="card-component">
-      {slotMap['header'] &&
-        <header className='card-header'>
-          <TitleCard
-            text={Title}
-            level={TitleLevel}
-            className='card-title'
-          />
-          {slotMap['header']}
-        </header>
+    <article className={cardStyles}
+      {...props}>
+      <header className='card-header'>
+        <CardTitle
+          text={title}
+          level={titleLevel}
+          className={'card-title'}/>
+        {slotMap['CardHeader']}
+      </header>
+      {imgSrc &&
+        <CardImg
+          alt={imgAlt}
+          className={'card-img'}
+          fit="cover"
+          ratio="none"
+          src={imgSrc} />
       }
-      <div className="card-description">
-        {slotMap['description']}
-      </div>
-      <div className="card-actions">
-        {slotMap['actions']}
-      </div>
+      <section className="card-content">
+        {slotMap['CardContent'] && (
+          <>
+            {slotMap['CardContent']}
+          </>
+        )}
+        </section>
+      {slotMap['CardActions'] &&
+        <div className="card-actions">
+          {slotMap['CardActions']}
+        </div>
+      }
+      {slotMap['CardFooter'] &&
+        <footer className="card-footer">
+          {slotMap['CardFooter']}
+        </footer>
+      }
     </article>
   );
 };
 
-export default Card;
+export default Card
---
+++ src/stories/composed/card/card.stories.tsx
@@ -0,0 +1,130 @@
+import { CSSProperties, ReactNode } from 'react';
+import { Card, CardProps } from '../../../components/card/Card'
+
+import './card-custom-styles.scss'
+
+//  Imports to composition example
+import Box from '../../../components/box/Box'
+import Button from '../../../components/button/Button'
+import Paragraph from '../../../components/paragraph/Paragraph'
+
+export default {
+  title: 'Components/Composed/Card',
+  component: Card,
+  parameters: {
+    vitest: {
+      // testFile: "ListComposed.test.tsx"
+    }
+  },
+  tags: ['autodocs'],
+  argType: {
+    className: {
+      name: 'className',
+      control: 'text'
+    },
+  },
+}
+
+export const Default = (args: CardProps) => (
+  <Card {...args}>
+    <div slot="CardHeader">
+      Custom header content
+    </div>
+    <p slot="CardContent">
+    Custom body content
+    </p>
+    <div slot="CardActions">
+      Custom action and call to actions section
+    </div>
+    <div slot="CardFooter">
+      Custom footer content
+    </div>
+  </Card>
+)
+
+Default.args = {
+  title: 'a card',
+  titleLevel: 4,
+  imgSrc: 'https://picsum.photos/300/100?random=1',
+  imgAlt: 'A random picture',
+  className: 'my-custom-styles'
+}
+
+export const CompositionExample = (args: CardProps) => ([
+  <Box className='my-custom-card-styles'>
+    <Card {...args}>
+      <div slot="CardHeader">
+        <Button
+          description="Menu alt"
+          icon="dots-vertical"
+          iconOnly
+          kind="rounded"
+          onClick={() => { }}
+          size="m"
+          text="Button"
+          variant="ghost"
+        />
+      </div>
+      <div slot="CardContent">
+        <Paragraph className='card-content-contented'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio maiores facilis atque ducimus corporis.
+        </Paragraph>
+      </div>
+      <div
+        slot="CardActions"
+        className='card-actions-contented'>
+        <Button
+          description="Menu alt"
+          onClick={() => { }}
+          size="m"
+          text="main action"
+          className="main-action"
+          variant="primary"
+        />
+        <Button
+          description="Menu alt"
+          onClick={() => { }}
+          size="m"
+          text="alt action"
+          className="alt-action"
+          variant="alt"
+        />
+      </div>
+      <div
+        slot="CardFooter"
+        className="card-footer-contented">
+        <Paragraph className='card-footer-legal'>
+          legal text Ⓡ
+        </Paragraph>
+      </div>
+    </Card>
+  </Box>
+])
+
+CompositionExample.args = {
+  unstyled: true,
+  title: 'a card',
+  titleLevel: 4,
+  imgSrc: 'https://picsum.photos/800/300?random=1',
+  imgAlt: 'A random picture',
+  className: 'my-custom-card-styles'
+}
+
+export const Unstyled = (args: CardProps) => (
+  <Card {...args}>
+    <div slot="CardHeader">Custom header content</div>
+    <p slot="CardContent">Custom body content</p>
+    <div slot="CardActions">Custom action and call to actions section</div>
+    <div slot="CardFooter">Custom footer content</div>
+  </Card>
+)
+
+Unstyled.args = {
+  unstyled: true,
+  title: 'a card',
+  titleLevel: 4,
+  imgSrc: 'https://picsum.photos/300/100?random=1',
+  imgAlt: 'A random picture',
+  className: 'your-custom-styles'
+}
+
