188fffe7a58d9358c6e2dca94548d86c65a3c63b
Dinael URDANETA <dinael@gmail.com>
2023-08-29 15:42:52 +0200

    📖 Add breadcrumb story

---
+++ src/stories/basic/breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,29 @@
+import { Breadcrumb, BreadcrumbProps } from '../../../components/breadcrumb/Breadcrumb'
+
+export default {
+  title: 'Components/Basic/Breadcrumb',
+  component: Breadcrumb,
+  parameters: {
+    vitest: {
+      testFile: "List.test.tsx"
+    }
+  },
+  tags: ['autodocs'],
+  argType: {
+  }
+}
+
+const breadcrumbTrace = [
+  { text: 'Home', url: '/' },
+  { text: 'Products', url: '/products' },
+  { text: 'Category', url: '/products/category' },
+  { text: 'Product Details', url: '/products/category/product-123' },
+]
+
+export const Default = (args: BreadcrumbProps) => (
+  <Breadcrumb {...args} />
+)
+
+Default.args = {
+  breadcrumbTrace: breadcrumbTrace
+}
