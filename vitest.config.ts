7ae78564a8c5c23a4486b2b2dee371fbb2f2003c
Dinael URDANETA <dinael@gmail.com>
2023-09-10 12:47:04 +0200

    ➕ Add vite-tsconfig-paths dependency to problem whit relative path whit Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths()
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
     coverage: {
       reportsDirectory: './src/tests/unit/coverage'
+    },
+    resolve: {
+      alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
     },
     reporters: [
       ['json', { 'file': 'coverage.json' }]
